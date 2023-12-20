637b7d63369341f540707c569f8375cffb616a00
Sukh Raj Singh <58319414+sukhraj-itech@users.noreply.github.com>
2023-12-20 16:53:01 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,8 @@
 from http.server import BaseHTTPRequestHandler
+a = 10
+b = 10
+
+c = b + a
 
 class handler(BaseHTTPRequestHandler):
 
@@ -6,5 +10,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello Shivam are Sukhraj, world, welcome to my python chapter1!'.encode('utf-8'))
+        self.wfile.write('Hello Shivam are Sukhraj,  {c} world, welcome to my python chapter1!'.encode('utf-8'))
         return
